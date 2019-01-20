4c94561554d385f4347795b7433c1843a0c88fa3
aelindwatson <aelindwatson@gmail.com>
2019-01-19 20:30:07 -0500

    Added checkbox and props

--- src/App.js
+++ src/App.js
@@ -3,12 +3,24 @@ import './App.css';
 import ToDo from './components/ToDo.js';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [
+        { description: 'Workout at OrangeTheory', isCompleted: true },
+        { description: 'Meditation for 20 minutes', isCompleted: false },
+        { description: 'Spanish homework', isCompleted: false },
+      ]
+    };
+  }
+
   render() {
     return (
       <div className="App">
        <ul>
-          <ToDo />
-          <ToDo />
+          { this.state.todos.map( (todo, index) =>
+            <ToDo key={ index } />
+          )}
         </ul>
       </div>
     );
